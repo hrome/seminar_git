c66185de9e774e9b58d867078fa03ff8d861b9b1
Roman Kovalev <kovalev@dostavista.ru>
2018-03-29 22:53:40 +0300

    Add hash_object.php

---
+++ hash_object.php
@@ -0,0 +1,31 @@
+#!/usr/bin/env php
+<?php
+
+$inputContent = file_get_contents('php://stdin');
+
+$options     = getopt('w');
+$writeObject = isset($options['w']);
+
+$type             = $argv[1] ?? 'blob';
+$contentSeparator = "\0";
+$typeSeparator    = ' ';
+
+$objectContent = $type . $typeSeparator . strlen($inputContent) . $contentSeparator . $inputContent;
+$objectSha1    = sha1($objectContent);
+
+echo $objectSha1;
+
+if ($writeObject) {
+    $objectContentCompressed = gzdeflate($objectContent, -1, ZLIB_ENCODING_DEFLATE);
+
+
+    $objectDirName  = substr($objectSha1, 0, 2);
+    $objectFileName = substr($objectSha1, 2);
+
+    $dirPath = '.git/objects/' . $objectDirName;
+    if (!mkdir($dirPath) && !is_dir($dirPath)) {
+        throw new \RuntimeException(sprintf('Directory "%s" was not created', $dirPath));
+    }
+
+    file_put_contents($dirPath . '/' . $objectFileName, $objectContentCompressed);
+}
